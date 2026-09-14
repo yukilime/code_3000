bd71ce0ff846a1ad5b3fd131b494a4c8ab32ad39
Anya <annamelnyk217@gmail.com>
2026-09-14 11:30:06 -0400

    changed learning_rate, n_estimators, max_depth

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,9 +10,9 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.1,
-        n_estimators=100,
-        max_depth=8,
+        learning_rate=0.05,
+        n_estimators=200,
+        max_depth=2,
         subsample=1,
         min_samples_leaf=1,
         random_state=seed
